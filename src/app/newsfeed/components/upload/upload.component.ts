50d0d63a0771e40ed27ae89c9db6c8523e5125a7
Mike <mikeyyg9@gmail.com>
2021-11-09 15:03:00 +0200

    Pretty posts

--- src/app/newsfeed/components/upload/upload.component.ts
+++ src/app/newsfeed/components/upload/upload.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/compat/storage';
-import { Observable } from 'rxjs'
+import { Observable, take } from 'rxjs';
 import { Post } from 'src/app/models/Post';
 import { PopupService } from 'src/app/services/popup.service';
 import { PostsService } from 'src/app/services/posts.service';
@@ -23,11 +23,11 @@ export class UploadComponent implements OnInit {
     postImmediately: true,
     notifyAt_ts: null,
     numberOfViewers: [],
-    postType: "post",
+    postType: 'post',
     isEvent: false,
     eventSubscribers: [],
-    imageURL: null
-  }
+    imageURL: ''
+  };
   loadingVisible = false;
   // main task
   task: AngularFireUploadTask;
@@ -41,11 +41,11 @@ export class UploadComponent implements OnInit {
 
   isHovering: boolean;
 
-  imagePath: string = undefined
+  imagePath: string = undefined;
 
-  isNew: boolean = true
+  isNew = true;
 
-  showSubscribersPopup = false
+  showSubscribersPopup = false;
 
   constructor(private storage: AngularFireStorage, private popup: PopupService, private service: PostsService,
     private route: ActivatedRoute, private router: Router, private location: Location) { }
@@ -53,19 +53,19 @@ export class UploadComponent implements OnInit {
   ngOnInit(): void {
     const uid = this.route.snapshot.paramMap.get('uid');
     if (uid) {
-      this.isNew = false
+      this.isNew = false;
       this.loadingVisible = true;
       this.service.getOne(uid).subscribe(post => {
-        this.post = post
-        this.post.isEvent = this.post.postType === "event"
+        this.post = post;
+        this.post.isEvent = this.post.postType === 'event';
         this.loadingVisible = false;
       }, error => {
-        console.log(error)
+        console.log(error);
         this.popup.error('Could not fetch post! If problem persists please contact support', () => {
           this.loadingVisible = false;
-          this.location.back()
-        })
-      })
+          this.location.back();
+        });
+      });
     }
   }
 
@@ -75,7 +75,7 @@ export class UploadComponent implements OnInit {
 
   startUpload(event: FileList) {
 
-    this.loadingVisible = true
+    this.loadingVisible = true;
 
     const file = event.item(0);
 
@@ -87,20 +87,20 @@ export class UploadComponent implements OnInit {
 
     const path = `posts/${new Date().getTime()}_${file.name.split(' ').join('')}`;
 
-    const meta = { tesT: 'case' }
+    const meta = { tesT: 'case' };
 
     // upload
-    this.task = this.storage.upload(path, file)
+    this.task = this.storage.upload(path, file);
 
     this.percentage = this.task.percentageChanges();
     this.task.snapshotChanges().subscribe(e => {
       const uploaded = e.bytesTransferred >= e.totalBytes;
       if (uploaded) {
-        this.downloadURL = e.ref.getDownloadURL()
-        this.imagePath = path
+        this.downloadURL = e.ref.getDownloadURL();
+        this.imagePath = path;
         this.loadingVisible = false;
       }
-    })
+    });
   }
 
   isActive(snapshot) {
@@ -109,92 +109,129 @@ export class UploadComponent implements OnInit {
 
 
   savePost() {
-    const isNew = this.isNew
+    const isNew = this.isNew;
     this.post.createdAt_ts = new Date().getTime();
-    this.post.lastUpdated_ts = null
-    this.post.postType = this.post.isEvent ? "event" : "post"
+    this.post.lastUpdated_ts = null;
+    this.post.postType = this.post.isEvent ? 'event' : 'post';
     if (this.downloadURL) {
       this.downloadURL.then(url => {
-        if(this.imagePath && url) {
-          this.post.imagePath = this.imagePath
+        if (this.imagePath && url) {
+          this.post.imagePath = this.imagePath;
           if (isNew) {
-            this.createPost(url)
+            this.createPost(url);
           } else {
-            this.updatePost(url)
+            this.updatePost(url);
           }
-          return
+          return;
         }
       })
         .catch(e => {
-          console.log(e)
-          this.popup.error('Error uploading file')
-        })
-      return
+          console.log(e);
+          this.popup.error('Error uploading file');
+        });
+      return;
     }
 
     if (isNew) {
-      this.createPost(null)
+      this.createPost(null);
     } else {
-      this.updatePost(null)
+      this.updatePost(null);
     }
   }
 
 
   private updatePost(url: string) {
-    if (url)
-      this.post.imageURL = url
+    if (url) {
+      this.post.imageURL = url;
+    }
     this.service.update([this.post]).then(() => {
-      this.popup.success('Post updated!')
+      this.popup.success('Post updated!');
     }).catch(e => {
-      console.log(e)
-      this.popup.error('Could not update post')
-    })
+      console.log(e);
+      this.popup.error('Could not update post');
+    });
   }
 
   onDateSelected($event) {
-    const ts = $event.getTime() as number
-    if(ts <= new Date().getTime()) {
-      this.popup.error('Please select a future date to publish the post')
+    const ts = $event.getTime() as number;
+    if (ts <= new Date().getTime()) {
+      this.popup.error('Please select a future date to publish the post');
       return;
     }
 
-    this.post.notifyAt_ts = ts
+    this.post.notifyAt_ts = ts;
 
-    console.log(new Date(ts))
+    console.log(new Date(ts));
   }
 
   private createPost(url: string) {
     if (url) {
-      this.post.imageURL = url
+      this.post.imageURL = url;
     }
     this.service.create([this.post]).then(() => {
       this.popup.success('Post created', () => {
-        this.location.back()
-      })
+        this.location.back();
+      });
     }, error => {
-      console.log(error)
-      this.popup.error('Could not create post, if problem persists please contact support')
-    })
+      console.log(error);
+      this.popup.error('Could not create post, if problem persists please contact support');
+    });
   }
 
   onPostImmediatelyClicked() {
-    const postImmediately = !this.post.postImmediately
-    this.post.notifyUsers = !postImmediately
-    this.post.visible = !postImmediately
+    const postImmediately = !this.post.postImmediately;
+    this.post.notifyUsers = !postImmediately;
+    this.post.visible = !postImmediately;
   }
 
   isEventClicked(isEvent: boolean) {
-    console.log(`is event` + isEvent)
+    console.log(`is event` + isEvent);
   }
 
   viewInterested() {
-    if(!this.post || this.post.eventSubscribers.length == 0) {
+    if (!this.post || this.post.eventSubscribers.length == 0) {
       this.popup.info('Nothing to show');
       return;
     }
 
     this.showSubscribersPopup = true;
 
+  }
+
+  deletePost() {
+    if (!this.post.uid) {
+      return;
+    }
+
+    this.popup.confirm('Are you sure you want to remove this post?', async confirmed => {
+      if (!confirmed) {
+        return;
+      }
+
+      if(this.post.imageURL) {
+        this.storage.refFromURL(this.post.imageURL).delete().pipe(take(1)).subscribe({
+          next: () => {
+            console.log('file deleted')
+          },
+          error: (e) =>  {
+            console.log('failed faild to be deleted')
+            console.log(e);
+          }
+        })
+      }
+      this.loadingVisible = true
+      this.service.remove([this.post])
+        .then(() => {
+          this.loadingVisible = false
+          this.popup.success('Post deleted', () => {
+            this.location.back()
+          })
+        }).catch((e) => {
+          this.loadingVisible = false
+          this.popup.error('Error while trying to delete post. Please contact support!');
+          console.log(e);
+        })
+    })
   }
 
 }
