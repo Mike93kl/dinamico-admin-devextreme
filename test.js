358fefe24da1ccb8500868e3cb5b54b00d5b880d
Mike <mikeyyg9@gmail.com>
2021-11-12 14:31:42 +0200

    Create schedule report generation

--- test.js
+++ test.js
@@ -1,2 +1,10 @@
-const a = "ASdSADSAD 1 2D 3Sd 4ad 5a ";
-console.log(a.split(' ').join(''))
+const a = new Map()
+
+a.set('a', 1)
+a.set('aa', {sessions: 'aa'})
+
+console.log(!a.get('aa'))
+
+for(const [k,v] of a) {
+    console.log(k,v)
+}
